5506579c9bd85c348d982a1f0df3a9c2b24eb619
Batuhan Demirbaş <batuhan@demirbas.co>
2023-04-19 11:50:16 +0300

    Added skeleton view animation

--- PortalGuide/Scene/Home/CharacterCollectionViewCell/Skeleton/SkeletonCollectionViewCell.swift
+++ PortalGuide/Scene/Home/CharacterCollectionViewCell/Skeleton/SkeletonCollectionViewCell.swift
@@ -2,16 +2,27 @@
 //  SkeletonCollectionViewCell.swift
 //  PortalGuide
 //
-//  Created by Batuhan on 7.04.2023.
+//  Created by Batuhan Demirbaş on 7.04.2023.
 //
 
 import UIKit
+import SkeletonView
 
 class SkeletonCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    @IBOutlet weak var statusView: UIView!
+    @IBOutlet weak var nameView: UIView!
+    @IBOutlet weak var genderView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        image.showAnimatedGradientSkeleton()
+        statusView.showAnimatedGradientSkeleton()
+        nameView.showAnimatedGradientSkeleton()
+        genderView.showAnimatedGradientSkeleton()
+
     }
 
 }
